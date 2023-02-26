679c5aae27111fa5a5bee595959edb494a7c3f90
evgeniy <evgen.maiorov@gmail.com>
2023-02-26 18:59:56 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Evgeny Mayorov");
+        System.out.println("26.02.2022");
+
+    }
+}
