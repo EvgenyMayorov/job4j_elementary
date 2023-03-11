6cebf977576240ebadc8b01996d3461d90f1d4da
evgeniy <evgen.maiorov@gmail.com>
2023-03-11 10:11:01 +0300

    1.2. Входные данные в тесте [#297750 #398934]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -7,7 +7,6 @@ public class X2Test {
 
     @Test
     public void whenA10B0C0X2Then40() {
-
         int a = 10;
         int b = 0;
         int c = 0;
@@ -15,12 +14,10 @@ public class X2Test {
         int expected = 40;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-
     }
 
     @Test
     public void whenA1B1C1X1Then3() {
-
         int a = 1;
         int b = 1;
         int c = 1;
@@ -28,12 +25,10 @@ public class X2Test {
         int expected = 3;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-
     }
 
     @Test
     public void whenA0B1C1X1Then2() {
-
         int a = 0;
         int b = 1;
         int c = 1;
@@ -41,12 +36,10 @@ public class X2Test {
         int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-
     }
 
     @Test
     public void whenA1B1C0X1Then2() {
-
         int a = 1;
         int b = 1;
         int c = 0;
@@ -54,12 +47,10 @@ public class X2Test {
         int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-
     }
 
     @Test
     public void whenA1B1C1X0Then1() {
-
         int a = 1;
         int b = 1;
         int c = 1;
@@ -67,6 +58,5 @@ public class X2Test {
         int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-
     }
 }
