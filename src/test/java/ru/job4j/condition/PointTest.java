a7ba8a4057f6b2948ba4c39240d6c94f067c77e7
evgeniy <evgen.maiorov@gmail.com>
2023-05-14 19:20:43 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -3,38 +3,34 @@ package ru.job4j.condition;
 import org.junit.Test;
 import org.junit.Assert;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PointTest {
 
     @Test
     public void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when31to63then3Dot6() {
         double expected = 3.6;
-        int x1 = 3;
-        int y1 = 1;
-        int x2 = 6;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(3, 1);
+        Point b = new Point(6, 3);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when40to27then7Dot28() {
         double expected = 7.28;
-        int x1 = 4;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 7;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(4, 0);
+        Point b = new Point(2, 7);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 }
