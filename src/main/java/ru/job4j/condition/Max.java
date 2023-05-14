6ba549770357d38d7a1a8f6dd10cbc24e227a74b
evgeniy <evgen.maiorov@gmail.com>
2023-05-14 20:52:32 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,16 +1,24 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
+    public static int max(int a, int b) {
+        return a >= b ? a : b;
+    }
+
+    public static int max(int a, int b, int c) {
+        return c >= Max.max(a, b) ? c : Max.max(a, b);
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        return d >= Max.max(c, Max.max(a, b)) ? d : Max.max(c, Max.max(a, b));
     }
 
     public static void main(String[] args) {
         int res = Max.max(3, 6);
         System.out.println(res);
-        res = Max.max(8, 8);
+        res = Max.max(8, 8, 20);
         System.out.println(res);
-        res = Max.max(89, 15);
+        res = Max.max(89, 15, 8, 99);
         System.out.println(res);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,19 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int a = 1;
+        int b = 2;
+        int result = Max.max(a, b);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax90To28Then90() {
-        int left = 90;
-        int right = 28;
-        int result = Max.max(left, right);
-        int expected = 90;
+    public void whenMax90To28To99Then99() {
+        int a = 90;
+        int b = 28;
+        int c = 99;
+        int result = Max.max(a, b, c);
+        int expected = 99;
         assertThat(result).isEqualTo(expected);
     }
 
@@ -32,10 +33,12 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax56To987Then987() {
-        int left = 56;
-        int right = 987;
-        int result = Max.max(left, right);
+    public void whenMax56To987To5To80Then987() {
+        int a = 56;
+        int b = 987;
+        int c = 5;
+        int d = 80;
+        int result = Max.max(a, b, c, d);
         int expected = 987;
         assertThat(result).isEqualTo(expected);
     }
